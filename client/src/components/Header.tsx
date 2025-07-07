d81b8899e68fd9ff8160119082d23212c5f216bb
royandresdev <royhuamanavila@gmail.com>
2025-07-06 22:25:36 -0500

    feat: actualizar componente Header para usar formateadDate y agregar InitialsAvatar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,18 +1,16 @@
-import { RectangleVertical } from "lucide-react"
+import { formateadDate } from "@/lib/utils"
+import InitialsAvatar from "./InitialsAvatar"
+import { useAuthStore } from "@/store/auth.store"
 
 function Header() {
+  const user = useAuthStore((state) => state.user);
+
   return (
     <header className="px-4 pt-2 pb-4 flex justify-between border-b border-var(--border) items-center">
       <h1 className="text-[#6b6fd4] font-bold text-xl">ConverSAFe</h1>
       <div className="flex items-center gap-4">
-        <span className="text-white bg-[#10b981] px-4 py-2 flex items-center justify-center rounded-lg gap-2">
-          <RectangleVertical className="w-4 h-4" /> IA Activa
-        </span>
-        <p>Equipo Alpha - <span>{new Date().toLocaleDateString("es-ES", {
-          day: "2-digit",
-          month: "short",
-          year: "numeric",
-        }).replace('.', '')}</span></p>
+        <p>Equipo Alpha - <span>{formateadDate()}</span></p>
+        <InitialsAvatar name={user?.username} size={32} className="rounded-md" />
       </div>
     </header>
   )
